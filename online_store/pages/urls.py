e1edde2b04017d7fb7c0a03d4db857b2502b964e
sgalle16 <sgalle16@eafit.edu.co>
2024-02-15 15:36:08 -0500

    C. adding products

--- online_store/pages/urls.py
+++ online_store/pages/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import HomeView, AboutView, ContactView
+from .views import (
+    HomeView, 
+    AboutView, 
+    ContactView, 
+    ProductIndexView,
+    ProductShowView
+    )
 
 app_name = "pages"
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
     path("about/", AboutView.as_view(), name='about'),
-    path("contact/", ContactView.as_view(), name='contact')
+    path("contact/", ContactView.as_view(), name='contact'),
+
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
 
 
 ]
