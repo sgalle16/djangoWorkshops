221f4f766eb85922c71fefb239619f1598683f22
sgalle16 <sgalle16@eafit.edu.co>
2024-02-22 09:53:53 -0500

    E. comment model

--- online_store/pages/admin.py
+++ online_store/pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Product, Comment
+
+admin.site.register(Product)
+admin.site.register(Comment)
--- online_store/pages/models.py
+++ online_store/pages/models.py
@@ -9,3 +9,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content = models.TextField()
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
