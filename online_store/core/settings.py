c0a0477fcbd59c371d122aa90a6cbe1d3a05a464
sgalle16 <sgalle16@eafit.edu.co>
2024-02-29 13:51:08 -0500

    C. service provider

--- online_store/core/settings.py
+++ online_store/core/settings.py
@@ -118,3 +118,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
