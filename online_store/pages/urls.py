5d3ca7ad3206189691f103167ea9056de7fc36fe
sgalle16 <sgalle16@eafit.edu.co>
2024-02-29 14:07:19 -0500

    D. add image storage functionality

--- online_store/pages/urls.py
+++ online_store/pages/urls.py
@@ -1,28 +1,35 @@
 from django.urls import path
+from .utils import ImageLocalStorage
 from .views import (
-    HomeView, 
-    AboutView, 
-    ContactView, 
+    HomeView,
+    AboutView,
+    ContactView,
     ProductIndexView,
     ProductShowView,
     ProductCreateView,
     CartView,
     CartRemoveAllView,
-    )
+    ImageViewFactory,
+)
 
 app_name = "pages"
 
 urlpatterns = [
+    # Pages
     path("", HomeView.as_view(), name='home'),
     path("about/", AboutView.as_view(), name='about'),
     path("contact/", ContactView.as_view(), name='contact'),
-
+    # Products
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-    
+    # Cart
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    # Images storage
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+
 
 ]
--- online_store/pages/views.py
+++ online_store/pages/views.py
@@ -158,3 +158,21 @@ class CartRemoveAllView(View):
             del request.session['cart_product_data']
 
         return redirect('pages:cart_index')
+
+
+# Image Storage
+
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('pages:image_index')
+
+    return ImageView
